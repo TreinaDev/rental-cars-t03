701a69340ec0fa1590b1303dad876b023d5915a1
Henrique Morato <riquemorato@gmail.com>
2020-04-27 12:31:19 -0300

    I18n nas validações

--- app/models/subsidiary.rb
+++ app/models/subsidiary.rb
@@ -1,8 +1,7 @@
 class Subsidiary < ApplicationRecord
   validates :name, :cnpj, :address, presence: true
   validates :name, :cnpj, uniqueness: true
-  validates :cnpj, format: { with: /\A^\d{2,3}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$\z/,
-                             message: 'não é válido' }
+  validates :cnpj, format: { with: /\A^\d{2,3}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$\z/ }
   validate :cnpj_must_be_valid
 
   private
@@ -10,6 +9,6 @@ class Subsidiary < ApplicationRecord
   def cnpj_must_be_valid
     return if CNPJ.valid?(cnpj, strict: true)
 
-    errors.add(:cnpj, 'não é válido')
+    errors.add(:cnpj, :invalid)
   end
 end
