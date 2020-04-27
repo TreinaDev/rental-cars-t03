7a0929131aeec6648a3d0c2c714aa72ab818af38
Henrique Morato <riquemorato@gmail.com>
2020-04-27 16:01:12 -0300

    Validação de modelo de carro

--- app/controllers/car_models_controller.rb
+++ app/controllers/car_models_controller.rb
@@ -14,8 +14,14 @@ class CarModelsController < ApplicationController
   end
 
   def create
-    @car_model = CarModel.create(car_model_params)
-    redirect_to @car_model
+    @car_model = CarModel.new(car_model_params)
+    if @car_model.save
+      redirect_to @car_model
+    else
+      @manufacturers = Manufacturer.all
+      @car_categories = CarCategory.all
+      render :new
+    end
   end
 
   private
